65b31d3f951ebc4198d845da271949b9b1869f17
istvank <istvank@gmx.de>
2020-02-03 10:34:53 +0100

    Adding commonjs transform

--- es-dev-server.config.js
+++ es-dev-server.config.js
@@ -1,7 +1,13 @@
 module.exports = {
   port: 8000,
   watch: true,
-  nodeResolve: true,
+  nodeResolve: {
+    browser: true,
+    mainFields: ['module', 'browser', 'main']
+  },
+  babelConfig: {
+    plugins: [["babel-plugin-transform-commonjs", {"onlyExports": true}]]
+  },
   dedupe: true,
   preserveSymlinks: true,
   appIndex: 'index.html',
